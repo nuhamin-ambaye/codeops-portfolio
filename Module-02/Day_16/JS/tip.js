2ab6c65f256e49aacc1e4d93856b69c7c85cf628
Nuhamin Ambaye <nuhamin1321@gmail.com>
2026-08-08 20:42:55 +0300

    tip.js is done.

--- Module-02/Day_16/JS/tip.js
+++ Module-02/Day_16/JS/tip.js
@@ -1,9 +1,12 @@
+// Accepting values (they should be accepted from the user)
 let  bill="2000"
 let partySize=5
 
+// Total and per person amount
 perPersonAmount=bill/partySize
 console.log(`The amount each person gonna pay is ${perPersonAmount}`)
 
+// Tiping
 if (bill>300){
     bill*=0.1
     console.log(`Tip=${bill}`)
@@ -13,6 +16,7 @@ else{
     console.log(`Tip=${bill}`)
 }
 
+// Service fee by switch
 let method="CBE"
 switch (method){
     case 'CBE':
@@ -21,7 +25,9 @@ switch (method){
     case 'Telebirr':
         console.log('Paid with Telebirr')
         break
+    case 'Abyssinia':
+        console.log('Paid with Abyssinia')
+        break
     default:
         console.log('Unknown method!')
 }
-
