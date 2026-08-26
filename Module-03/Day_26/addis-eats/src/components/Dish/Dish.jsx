abcc493afe51e06767f6d9aaebe8d948bb2b2c69
Nuhamin Ambaye <nuhamin1321@gmail.com>
2026-08-26 23:34:44 +0300

    Done

---
+++ Module-03/Day_26/addis-eats/src/components/Dish/Dish.jsx
@@ -0,0 +1,12 @@
+import "./Dish.css"
+
+function Dish({ name, price }) {
+return (
+<div className="dish">
+    <h3>{name}</h3>
+    <p>{price} ETB</p>
+</div>
+);
+}
+
+export default Dish;
---
+++ Module-03/Day_26/addis-eats/src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,11 @@
+import "./Sidebar.css"
+
+function Sidebar() {
+  return (
+        <aside className="sidebar">
+        <h2>All foods are here.</h2>
+        </aside>
+    );
+}
+
+export default Sidebar;
